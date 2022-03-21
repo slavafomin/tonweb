c4d571b9fb5d16f9eb91674189cbe0c4dab10e01
Slava Fomin II <slava@fomin.io>
2022-03-21 17:50:34 +0300

    Fixed TS-configs
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- src/index.ts
+++ src/index.ts
@@ -271,7 +271,7 @@ const NFT = {
 
 // This version is updated by build script automatically,
 // do not edit it or change the expression format.
-const version = '0.0.35-2';
+const version = '0.0.35-beta.0';
 
 
 export default class TonWeb {
