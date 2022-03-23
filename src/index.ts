dde2a7ac7f1aae35d6bd5a72534c8d3d73c3f50b
Slava Fomin II <slava@fomin.io>
2022-03-23 20:31:03 +0300

    Prepared a release
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- src/index.ts
+++ src/index.ts
@@ -102,7 +102,6 @@ export {
 import { Wallets } from './contract/wallet/wallets';
 
 export {
-    DeployMethod,
     ExternalMessage,
     SeqnoMethod,
     SeqnoMethodResult,
@@ -271,7 +270,7 @@ const NFT = {
 
 // This version is updated by build script automatically,
 // do not edit it or change the expression format.
-const version = '0.0.35-beta.0';
+const version = '0.0.35-beta.1';
 
 
 export default class TonWeb {
