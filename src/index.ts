9d77383277252cb35592a6d39fcdd9dae3758f97
Slava Fomin II <slava@fomin.io>
2022-03-27 22:51:35 +0300

    Prepared a release
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- src/index.ts
+++ src/index.ts
@@ -601,7 +601,7 @@ const NFT = {
 
 // This version is updated by build script automatically,
 // do not edit it or change the expression format.
-const version = '0.0.35';
+const version = '0.0.35-beta.3';
 
 
 export default class TonWeb {
